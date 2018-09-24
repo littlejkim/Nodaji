49025778ca6ebfbb707172078086ad225567ba2b
Jung Eek Bang <jungeekbang@Jungs-Mac.local>
2018-09-23 17:39:17 -0700

    dsalkf

---
+++ Nodaji/Nodaji/View Controllers/ProtectedPageLayoutController.swift
@@ -0,0 +1,35 @@
+//
+//  ProtectedPageLayoutController.swift
+//  Nodaji
+//
+//  Created by Jung Eek Bang on 9/23/18.
+//  Copyright © 2018 Nodaji. All rights reserved.
+//
+
+import UIKit
+
+class ProtectedPageLayoutController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
